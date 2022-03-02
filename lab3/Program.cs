a1025b110297266bf92b56b6c2d10c2ead626e33
Jakub Kurek <jakub.kurek1@microsoft.wsei.edu.pl>
2022-03-02 09:51:27 +0100

    lab1

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,14 +1,103 @@
 ﻿using System;
+using static lab1.Person;
 
-namespace lab3
+namespace lab1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = Person.Of("Kuba");
+            Console.WriteLine(person.FirstName);
+            Money money1 = Money.Of(1, Currency.PLN);
         }
-
-
     }
-}
+        class Person
+        {
+
+            private string _firstName;
+
+            private Person(string firstName)
+            {
+                _firstName = firstName;
+            }
+
+            public static Person Of(string firstName)
+            {
+                if (firstName.Length <= 2)
+                {
+                    return new Person(firstName);
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Imię jest zbyt krótkie");
+                }
+            }
+
+            public string FirstName
+            {
+                get
+                {
+                    return FirstName;
+                }
+                set
+                {
+                    if (value.Length >= 2)
+                    {
+                        _firstName = value;
+                    }
+                }
+            }
+
+            public enum Currency
+            {
+                PLN = 1,
+                USD = 2,
+                EUR = 3
+            }
+
+            public class Money
+            {
+                private readonly decimal _value;
+
+                private readonly Currency _currency;
+
+                private Money(decimal value, Currency currency)
+                {
+                    _value = value;
+                    _currency = currency;
+                }
+
+                public static Money? Of(decimal value, Currency currency)
+                {
+                    if (value < 0)
+                    {
+                        return value < 0 ? null : new Money(value, currency);
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("Nie można wykonać działania");
+                    }
+                }
+            public static Money? OfWithException(decimal value, Currency currency)
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    return new Money(value, currency);
+                }
+
+            }
+            public static Money operator *(Money money, decimal value)
+            {
+                return Money.Of(money._value * value, money._currency);
+            }
+            // money * 5 --> *(money, 5)
+            // 5 * money --> *(5, money)
+
+        }
+        }
+    }
