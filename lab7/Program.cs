8eacfbb331128358559638d5375e594460ecb60f
Jakub Kurek <jakub.kurek1@microsoft.wsei.edu.pl>
2022-04-20 12:02:02 +0200

    lab7

---
+++ lab7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab7
+{
+    delegate double Operation(double a, double b);
+    class Program
+    {
+        static double Addition (double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Mul(double x, double y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Operation add = Addition;
+            double result = add.Invoke(3, 5);
+            Console.WriteLine(result);
+
+            add = Mul;
+            Console.WriteLine(add.Invoke(3, 5));
+        }
+    }
+}
